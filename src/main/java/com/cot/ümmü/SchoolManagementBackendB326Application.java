7a7d5e67e779298937353bb24aa31abf66e43666
ummuhanı çot <ummuhancot@gmail.com>
2025-02-07 10:21:53 +0300

    backend project :)

---
+++ src/main/java/com/cot/ümmü/SchoolManagementBackendB326Application.java
@@ -0,0 +1,14 @@
+package com.cot.ümmü;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementBackendB326Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SchoolManagementBackendB326Application.class, args);
+  }
+
+
+}
